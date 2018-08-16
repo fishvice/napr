d7b7762226eebd27406e4f145534006c3de3bb5f
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2018-08-16 14:00:16 +0000

    added argument create_date in nap_logbook

--- R/nap_logbook.R
+++ R/nap_logbook.R
@@ -1,5 +1,5 @@
 #' @export
-nap_logbook <- function(con, convert = TRUE) {
+nap_logbook <- function(con, convert = TRUE, create_date = FALSE) {
   q <- nap_tbl(con, "logbook")
 
   if(convert) {
@@ -10,6 +10,15 @@ nap_logbook <- function(con, convert = TRUE) {
                     day = as.numeric(day),
                     hour = as.numeric(hour),
                     min = as.numeric(min))
+  }
+
+  if(create_date) {
+    q <-
+      q %>%
+      dplyr::mutate(date = to_date(ifelse(day <= 31 & month <= 12,
+                                          concat(concat(concat(concat(year, '-'), month), '-'), day),
+                                          "1958-24-12"),
+                                   'yyyy-mm-dd'))
   }
 
   return(q)
